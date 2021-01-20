ea101f524a1bae87ca1be648dd6fbc89a6300f14
senabalbinar <77548232+senabalbinar@users.noreply.github.com>
2021-01-20 22:06:02 +0300

    Add project files.

---
+++ OOP/Product.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace OOP
+{
+    //snippet
+    class Product
+    {
+
+        //CRUD operasyonları create update 
+        //ana anahtar
+        public int Id { get; set; }
+        public int CategoryId { get; set; }
+        public string ProductName { get; set; }
+        public double UnitPrice { get; set; }
+
+        //stok adedi
+        public int UnitsInStock { get; set; }
+
+    }
+}
---
+++ OOP/ProductManager.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace OOP
+{
+    class ProductManager
+    {
+        //string ad= Product product--> string yazsan tek bi metin ister
+        //encapsulation product cinsinden bi şey yaz
+        public void Add(Product product)
+        {
+
+        }
+    }
+}
